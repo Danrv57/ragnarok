798abb2768895db4553002268508d638adfe4e49
TriggerHappy55 <marcos.rodriguez.nistal@outlook.com>
2026-02-28 19:55:02 +0100

    Pruebas

--- API/main.py
+++ API/main.py
@@ -74,7 +74,7 @@ def cargar_contrasenas_vulneradas():
     contrasenas_vulneradas = set()
     
     # Buscar todos los archivos .txt en la carpeta rockyou
-    archivos_txt = glob.glob('../rockyou/*.txt') <- Pruebas locales
+    archivos_txt = glob.glob('../rockyou/*.txt')
     #ROCKYOU_DIR = Path(__file__).parent.parent / "rockyou"
     #iarchivos_txt = list(ROCKYOU_DIR.glob("*.txt"))
     
